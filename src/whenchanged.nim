9a25a84cff56f3d16116e5822ed5f8a964502b44
Łukasz Maksymczuk <lukasz.maksymczuk@gmail.com>
2023-04-25 22:17:43 +0200

    run command when the file changed

--- src/whenchanged.nim
+++ src/whenchanged.nim
@@ -1,5 +1,6 @@
 import std/os
-import std/sequtils
+import std/osproc
+import std/strutils
 import std/posix
 import std/inotify
 
@@ -7,29 +8,32 @@ import std/inotify
 func `$`(ev: ptr InotifyEvent): string =
   $cast[cstring](ev.name.addr) & " " & $ev.mask & " " & $ev.len
 
-func pathParts(path: string): iterator(): string= 
-  return iterator(): string = 
-    var rest = path
-    while rest != "" and rest != ".":
-      let (newrest, tail) = rest.splitPath
-      rest = newrest
-      yield tail
+iterator pathParts(path: string): string= 
+  var rest = path
+  while rest != "" and rest != ".":
+    let (newrest, tail) = rest.splitPath
+    rest = newrest
+    yield tail
 
 func isAnyHidden(path: string): bool =
-  path.pathParts.anyIt(it.isHidden)
+  for part in path.pathParts:
+    if part.isHidden or part == "__pycache__":
+      return true
+  return false
 
 proc main() =
+  let toExecute = commandLineParams()
   let inoty = inotify_init()
   for filename in os.walkDirRec(".", yieldFilter={pcDir}):
     if not filename.isAnyHidden:
-      let fullfn = getCurrentDir() / filename
       if filename.dirExists:
         discard inotify_add_watch(inoty, filename.cstring, IN_CLOSE_WRITE)
 
   var evs = newSeq[byte](8192)
   while (let n = read(inoty, evs[0].addr, 8192); n) > 0:
     for e in inotify_events(evs[0].addr, n):
-      echo $e
+      echo "got event: " & $e
+      discard execCmd(toExecute.join(" "))
 
 when isMainModule:
   main()
